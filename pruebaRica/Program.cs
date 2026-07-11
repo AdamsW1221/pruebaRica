ba76746ec173f51ded2fd3600f38649e1da7c13b
AdamsW1221 <adamswever122132@gmail.com>
2026-07-11 00:08:23 -0400

    commit inicial

--- pruebaRica/Program.cs
+++ pruebaRica/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
